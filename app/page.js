bad35d1b1d4e8967831e08827ee127260fbf97c5
coordova <grcordova@gmail.com>
2023-12-20 21:11:03 -0400

    wfood - Footer

--- app/page.js
+++ app/page.js
@@ -1,4 +1,5 @@
 import About from "@/components/About";
+import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import Hero from "@/components/Hero";
 import Map from "@/components/Map";
@@ -15,8 +16,9 @@ export default function Home() {
 			<Reservation />
 			<About />
 			<Map />
+			<Footer />
 			{/* <StyKleGuide /> */}
-			<div className="h-[4000px]"></div>
+			{/* <div className="h-[4000px]"></div> */}
 		</main>
 	);
 }
